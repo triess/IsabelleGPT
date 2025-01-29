ae73c73961348d214d79b81cbdc552ab47b1fec7
Tim Rieß <timischaf12@gmail.com>
2025-01-29 13:48:48 +0100

    Update temp.thy

--- temp.thy
+++ temp.thy
@@ -6,21 +6,21 @@ begin
 (* Definition 2. An argumentation framework is a pair
 AF = (AR, attacks)
 where AR is a set of arguments, and attacks is a binary relation on AR, i.e.
-attacks ⊆ AR x AR.
+attacks \<subseteq> AR x AR.
 For two arguments A and B, the meaning of attacks(A, B) is that A represents
 an attack against B.
 *)
 
-typedef ('v) argumentation_framework = "{(V :: 'v set, E :: ('v × 'v) set). E ⊆ V × V}" by auto
+typedef ('v) argumentation_framework = "{(V :: 'v set, E :: ('v \<times> 'v) set). E \<subseteq> V \<times> V}" by auto
 
-definition arguments :: "('v) argumentation_framework ⇒ 'v set" where
+definition arguments :: "('v) argumentation_framework \<Rightarrow> 'v set" where
   "arguments G = fst (Rep_argumentation_framework G)"
 
-definition attack_relations :: "('v) argumentation_framework ⇒ ('v × 'v) set" where
+definition attack_relations :: "('v) argumentation_framework \<Rightarrow> ('v \<times> 'v) set" where
   "attack_relations G = snd (Rep_argumentation_framework G)"
 
-definition attacks :: "('v) argumentation_framework ⇒ 'v ⇒ 'v ⇒ bool" where
-  "attacks G a b ⟷ (a, b) ∈ attack_relations G"
+definition attacks :: "('v) argumentation_framework \<Rightarrow> 'v \<Rightarrow> 'v \<Rightarrow> bool" where
+  "attacks G a b \<longleftrightarrow> (a, b) \<in> attack_relations G"
 
 (* Example 3 (Continuation of Example 1). The exchange between I and A can be
 represented by an argumentation framework (AR, attacks) as follows: AR =
@@ -35,74 +35,74 @@ definition example_af :: "string argumentation_framework" where
 (* Definition 5. A set S of arguments is said to be conflict-free if there are no
 arguments A and B in S such that A attacks B. *)
 
-definition conflict_free :: "('v) argumentation_framework ⇒ 'v set ⇒ bool" where
-  "conflict_free G S ⟷ (∀a b. a ∈ S ∧ b ∈ S ⟶ ¬attacks G a b)"
+definition conflict_free :: "('v) argumentation_framework \<Rightarrow> 'v set \<Rightarrow> bool" where
+  "conflict_free G S \<longleftrightarrow> (\<forall>a b. a \<in> S \<and> b \<in> S \<longrightarrow> \<not>attacks G a b)"
 
 (* Definition 6.
-(1) An argument A ∈ AR is acceptable with respect to a set S of arguments iff
-for each argument B ∈ AR: if B attacks A then B is attacked by S.
+(1) An argument A \<in> AR is acceptable with respect to a set S of arguments iff
+for each argument B \<in> AR: if B attacks A then B is attacked by S.
 (2) A conflict-free set of arguments S is admissible iff each argument in S is
 acceptable with respect to S. *)
 
-definition acceptable :: "('v) argumentation_framework ⇒ 'v ⇒ 'v set ⇒ bool" where
-  "acceptable G A S ⟷ (∀B. B ∈ arguments G ∧ attacks G B A ⟶ (∃C. C ∈ S ∧ attacks G C B))"
+definition acceptable :: "('v) argumentation_framework \<Rightarrow> 'v \<Rightarrow> 'v set \<Rightarrow> bool" where
+  "acceptable G A S \<longleftrightarrow> (\<forall>B. B \<in> arguments G \<and> attacks G B A \<longrightarrow> (\<exists>C. C \<in> S \<and> attacks G C B))"
 
-definition admissible :: "('v) argumentation_framework ⇒ 'v set ⇒ bool" where
-  "admissible G S ⟷ conflict_free G S ∧ (∀A. A ∈ S ⟶ acceptable G A S)"
+definition admissible :: "('v) argumentation_framework \<Rightarrow> 'v set \<Rightarrow> bool" where
+  "admissible G S \<longleftrightarrow> conflict_free G S \<and> (\<forall>A. A \<in> S \<longrightarrow> acceptable G A S)"
 
 (* Definition 7. A preferred extension of an argumentation framework AF is a
 maximal (with respect to set inclusion) admissible set of AF. *)
 
-definition preferred_extension :: "('v) argumentation_framework ⇒ 'v set ⇒ bool" where
-  "preferred_extension G S ⟷ admissible G S ∧ (∀T. admissible G T ∧ S ⊆ T ⟶ S = T)"
+definition preferred_extension :: "('v) argumentation_framework \<Rightarrow> 'v set \<Rightarrow> bool" where
+  "preferred_extension G S \<longleftrightarrow> admissible G S \<and> (\<forall>T. admissible G T \<and> S \<subseteq> T \<longrightarrow> S = T)"
 
 
 (* 
 Lemma 10 (Fundamental Lemma). Let S be an admissible set of arguments, and A
 and A’ be arguments which are acceptable with respect to S. Then
-(1) S’ = S ∪ {A} is admissible, and
+(1) S’ = S \<union> {A} is admissible, and
 (2) A’ is acceptable with respect to S’.
 Proof. (1) We need only to show that S’ is conflict-free. Assume the contrary.
-Therefore, there exists an argument B ∈ S such that either A attacks B or B
+Therefore, there exists an argument B \<in> S such that either A attacks B or B
 attacks A. From the admissibility of S and the acceptability of A, there is an
 argument B’ in S such that B’ attacks B or B’ attacks A. Since S is conflict-free,
 it follows that B’ attacks A. But then there is an argument B” in S such that B”
 attacks B’. Contradiction!
-(2) Obvious. □
+(2) Obvious. \<box>
  *)
 lemma fundamental_lemma:
   assumes "admissible G S" 
     and "acceptable G A S" 
     and "acceptable G A' S"
-  shows "admissible G (S ∪ {A})" 
-    and "acceptable G A' (S ∪ {A})"
+  shows "admissible G (S \<union> {A})" 
+    and "acceptable G A' (S \<union> {A})"
 proof -
-  have conflict_free_SA: "conflict_free G (S ∪ {A})"
+  have conflict_free_SA: "conflict_free G (S \<union> {A})"
   proof (rule ccontr)
-    assume "¬ conflict_free G (S ∪ {A})"
-    then obtain B where "B ∈ S ∪ {A}" and "attacks G B A ∨ attacks G A B"
+    assume "\<not> conflict_free G (S \<union> {A})"
+    then obtain B where "B \<in> S \<union> {A}" and "attacks G B A \<or> attacks G A B"
       by (metis (no_types, lifting) Un_insert_right admissible_def assms(1) conflict_free_def insert_iff sup_bot_right) 
-    then consider (1) "B ∈ S" "attacks G B A" | (2) "B ∈ S" "attacks G A B" 
+    then consider (1) "B \<in> S" "attacks G B A" | (2) "B \<in> S" "attacks G A B" 
       | (3) "B = A" "attacks G A B" 
       by auto
     then show False
     proof cases
       case 1
-      then obtain B' where "B' ∈ S" and "attacks G B' B" using acceptable_def assms(2) sorry
-      moreover have "¬ attacks G B' A" 
-        using assms(1) `B' ∈ S` conflict_free_def
+      then obtain B' where "B' \<in> S" and "attacks G B' B" using acceptable_def assms(2) sorry
+      moreover have "\<not> attacks G B' A" 
+        using assms(1) `B' \<in> S` conflict_free_def
         by (metis "1"(1) admissible_def calculation(2)) 
       ultimately show False 
         using 1
         by (meson admissible_def assms(1) conflict_free_def) 
     next
       case 2
-      then obtain B' where "B' ∈ S" and "attacks G B' A" using assms(2) acceptable_def sorry
-      then obtain B'' where "B'' ∈ S" and "attacks G B'' B'" 
+      then obtain B' where "B' \<in> S" and "attacks G B' A" using assms(2) acceptable_def sorry
+      then obtain B'' where "B'' \<in> S" and "attacks G B'' B'" 
         using assms(1) admissible_def acceptable_def sorry
-      moreover have "¬ attacks G B'' B'" 
-        using assms(1) `B'' ∈ S` conflict_free_def
-        by (metis ‹B' ∈ S› admissible_def) 
+      moreover have "\<not> attacks G B'' B'" 
+        using assms(1) `B'' \<in> S` conflict_free_def
+        by (metis \<open>B' \<in> S\<close> admissible_def) 
       ultimately show False 
         by blast
     next
@@ -111,26 +111,26 @@ proof -
         using assms(2) acceptable_def sorry
     qed
   qed
-  moreover have "∀x∈S ∪ {A}. acceptable G x (S ∪ {A})"
+  moreover have "\<forall>x\<in>S \<union> {A}. acceptable G x (S \<union> {A})"
   proof
     fix x
-    assume "x ∈ S ∪ {A}"
-    then show "acceptable G x (S ∪ {A})"
+    assume "x \<in> S \<union> {A}"
+    then show "acceptable G x (S \<union> {A})"
     proof
-      assume "x ∈ S"
-      then show "acceptable G x (S ∪ {A})"
+      assume "x \<in> S"
+      then show "acceptable G x (S \<union> {A})"
         using assms(1) admissible_def sorry
     next
       assume "x = A"
-      then show "acceptable G x (S ∪ {A})"
+      then show "acceptable G x (S \<union> {A})"
         using assms(2) by blast
     qed
   qed
-  ultimately show "admissible G (S ∪ {A})"
+  ultimately show "admissible G (S \<union> {A})"
     by (simp add: admissible_def)
-  moreover have "acceptable G A' (S ∪ {A})"
+  moreover have "acceptable G A' (S \<union> {A})"
     using assms(3) by blast
-  ultimately show "acceptable G A' (S ∪ {A})"
+  ultimately show "acceptable G A' (S \<union> {A})"
     by simp
 qed
 end
